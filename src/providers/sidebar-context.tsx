93f9871822130f9aaf2f76d80a8320b0c491c0b0
Gaston Franco <gastonfr24@gmail.com>
2024-04-21 00:52:49 -0300

    local storage sidebar

--- src/providers/sidebar-context.tsx
+++ src/providers/sidebar-context.tsx
@@ -1,22 +1,41 @@
 'use client'
 
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 interface SidebarContextType {
   isCollapsed: boolean;
   toggleSidebarcollapse: () => void;
 }
 
-const initialValue: SidebarContextType = { isCollapsed: false, toggleSidebarcollapse: () => {} };
+const initialValue: SidebarContextType = {
+  isCollapsed: false,
+  toggleSidebarcollapse: () => {},
+};
 
 const SidebarContext = createContext<SidebarContextType>(initialValue);
 
-const SidebarProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [isCollapsed, setCollapse] = useState<boolean>(false);
+const SidebarProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  // Obtener el estado inicial del localStorage, si existe
+  const initialState = localStorage.getItem("sidebarState");
+  const [isCollapsed, setCollapse] = useState<boolean>(
+    initialState ? JSON.parse(initialState).isCollapsed : false
+  );
 
   const toggleSidebarcollapse = () => {
-    setCollapse((prevState) => !prevState);
+    setCollapse((prevState) => {
+      const newState = !prevState;
+      // Guardar el nuevo estado en el localStorage
+      localStorage.setItem("sidebarState", JSON.stringify({ isCollapsed: newState }));
+      return newState;
+    });
   };
+
+  useEffect(() => {
+    // Actualizar el localStorage cada vez que el estado cambie
+    localStorage.setItem("sidebarState", JSON.stringify({ isCollapsed }));
+  }, [isCollapsed]);
 
   return (
     <SidebarContext.Provider value={{ isCollapsed, toggleSidebarcollapse }}>
